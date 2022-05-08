99ed1c83e83f203f6c7e0aec47b68bc956fefaad
m-dzn <88yangkh@gmail.com>
2022-05-08 22:25:35 +0900

    :truck: Home 페이지 코드 pages 디렉토리로 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { BubblyButton } from "@/components";
 
-function App() {
+import { Home } from "@/app/pages";
+
+const App = () => {
     return (
-        <div>
-            <BubblyButton />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { BubblyButton } from "@/components";
+
+export const Home = () => {
+    return (
+        <div>
+            <BubblyButton />
+        </div>
+    );
+};
