8ad761de51e0e05b01cd8910aa4a966e1ab3f0bb
m-dzn <88yangkh@gmail.com>
2022-05-25 20:01:28 +0900

    :lipstick: [GlobalStyle] body 마진 제거

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 
-import { Home } from '@/app/pages';
+import { GlobalStyles } from '@/app/styles';
 export const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes>
         <Route index element={<Home />} />
       </Routes>
