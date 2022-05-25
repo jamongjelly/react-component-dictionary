3324a3f6feb6ec8229bd85c4d7baa4113e8a29b2
m-dzn <88yangkh@gmail.com>
2022-05-25 19:33:28 +0900

    :lipstick: [홈] 페이지 패딩 간격 수정

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { BubblyButton } from '@/components';
 
 export const Home = () => {
   return (
-    <div>
+    <Container>
       <BubblyButton />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.main`
+  min-height: 100%;
+  padding: 40px;
+`;
